7f159f66270d477ed0589d7d363a09b7e144fabb
David Zambo <dave@dcmf.io>
2021-11-14 09:40:54 +0100

    feat: implement sorting, filtering and pagination

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,9 +1,17 @@
 export const feed = async (parent, args, context) => {
+  const { skip, take, orderBy } = args;
   const where = args.filter ? {
     OR: [
       { description: { contains: args.filter } },
       { url: { contains: args.filter } }
     ]
   } : {}
-  return await context.prisma.link.findMany({ where })
+
+  const links = await context.prisma.link.findMany({ where, skip, take, orderBy })
+  const count = await context.prisma.link.count({where});
+
+  return {
+    links,
+    count
+  }
 }
